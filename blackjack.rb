6de64b3ac7f9e74f2387c3771d4aea0131aed7da
Jong Suk Park <jjong7345@gmail.com>
2013-10-04 00:59:09 -0400

    blackjack

--- blackjack.rb
+++ blackjack.rb
@@ -20,7 +20,7 @@ deck = suffleDeck
   player << deck.pop()
 end
 
-#player = [["Diamond", "A"], ["Diamond", "6"]]
+player = [["Diamond", "A"], ["Diamond", "6"]]
 
 2.times do
   dealer << deck.pop()
@@ -28,28 +28,30 @@ end
 
 def total(cards)
   t = 0 
-  ace = 0
-  cards.each do |c|
+  temp = []
+  temp2 = []
+  cards.each do |c| 
+    if c[1] != "A"
+      temp << c
+    else
+      temp2 << c
+    end    
+  end
+  temp = temp.concat(temp2)
+
+  temp.each do |c|
     if (c[1] == "J") || (c[1] == "Q") || (c[1] == "K")
       t = t + 10
     elsif (c[1] == "A")
-      ace = ace + 1
-      #if (t + 11) > 21
-       # t = t + 1
-      #else 
-       # t = t +11
-      #end
+      if (t + 11) > 21
+        t = t + 1
+      else 
+        t = t +11
+      end
     else
       t = t + c[1].to_i
     end
   end
-  ace.times {
-    if (t + 11) > 21
-     t = t + 1
-    else 
-     t = t +11
-    end
-  }
   return t   
 end
 
